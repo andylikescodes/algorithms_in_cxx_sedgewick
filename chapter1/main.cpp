9d9af4ece2d32ecccdcc3581832800326c147492
Dehua Liang <andylikescodes@gmail.com>
2017-04-13 09:20:31 -0700

    studied path compression by halfing on weighted quick union algorithm

--- chapter1/main.cpp
+++ chapter1/main.cpp
@@ -5,12 +5,13 @@ static const int N = 1000;
 void quickfind();
 void quickunion();
 void weighted_quickunion();
+void path_compression_wquickunion();
 
 int main(int argc, char *argv[])
 {
 	if (std::string(argv[1]) == "quickfind") {quickfind();}
 	if (std::string(argv[1]) == "quickunion") {quickunion();}
-	if (std::string(argv[1]) == "weighted") {weighted_quickunion();} 
+	if (std::string(argv[1]) == "weighted") {weighted_quickunion();}
 }
 
 void quickfind()
@@ -51,10 +52,34 @@ void weighted_quickunion()
 		for (i=p; i!=id[i]; i=id[i]);
 		for (j=q; j!=id[j]; j=id[j]);
 		if (i==j) continue;
+		// improve the traves time by keeping
+		// track of the tree size and only
+		// add to the smaller side
 		if (sz[i] < sz[j])
 			{id[i] = j; sz[j] += sz[i];}
 		else
 			{id[j] = i; sz[i] += sz[j];}
 		std::cout << " " << p << " " << q << std::endl;
+	}
+}
+
+void path_compression_wquickunion()
+{
+	int i, j, p, q id[N], sz[N];
+	for (i=0; i<N; i++)
+		{ id[i]=i; sz[i]=1; }
+	while (std::cin >> p >> q)
+	{
+		//path compression by halving
+		for (i=p; i!=id[i]; i=id[i]);
+			id[i] = id[id[i]];
+		for (j=q; i!=id[j]; j=id[j]);
+			id[j] = id[id[j]];
+		if (i=j) continue;
+		if (sz[i] < sz[j])
+			{id[i] = j; sz[j] += sz[i];}
+		else
+			{id[j] = i; sz[i] += sz[j];}
+		std::cout << " " << p << " " << std::endl;
 	}
 }
